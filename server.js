035a33ed159ce3d286a52a7fdabe1fac088e04da
Faaiz uddin <Faaizuddin00@gmail.com>
2025-10-16 15:24:39 +0500

    Central Error Handling

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 dotenv.config();
 connectDB();
@@ -12,12 +13,13 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
+
 // Routes
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>  console.log(`Server running at http://localhost:${PORT}`));
