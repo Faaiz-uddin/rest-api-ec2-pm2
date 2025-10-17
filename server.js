4f5b343722d8091e8069ec4e1e5ca2b42e20a7eb
Faaiz uddin <Faaizuddin00@gmail.com>
2025-10-17 17:17:30 +0500

    Updated Msg

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.use("/api/auth", authRoutes);
 
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>  console.log(`Server running at http://localhost:${PORT}`));
+app.listen(PORT, () =>  console.log(`Servers running at http://localhost:${PORT}`));
